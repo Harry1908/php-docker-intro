537cc5f74e9cb9bcfab477bafd1f9ff526b1472f
Harry1908 <82804236+Harry1908@users.noreply.github.com>
2021-06-17 16:21:49 +0200

    little new changes

---
+++ www/public/Header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-wooteridth, initial-scale=1.0">
+  <title>PHP Include Exercise</title>
+</head>
+
+<body>
+
+<header>
+   <nav>
+      <ul> 
+         <li><a href="#home">HOME</a></li>
+         <li><a href="#menue">MENUE</a></li>
+         <li><a href="#order">BESTELLUNG</a></li>
+      </ul>
+   </nav>
+</header>
+
+</body>
